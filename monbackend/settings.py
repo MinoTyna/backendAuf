c7826cbef9267dc9d4cea7c276d850fadd0e04b3
ChristineRazafisoa <christinerasoanambinina@gmail.com>
2025-12-08 20:59:35 +0300

    fix: database settings

--- monbackend/settings.py
+++ monbackend/settings.py
@@ -145,16 +145,19 @@ REST_FRAMEWORK = {
 #     }
 # }
 
+
+
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'auf-sarl',
-            'USER': 'postgres',
-            'PASSWORD': '1234',
-            'HOST': 'localhost',
-            'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
+
 
 
 
