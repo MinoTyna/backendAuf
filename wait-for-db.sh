1346cbd57e3d0c0ae773f72fc4babceb72acc08e
ChristineRazafisoa <christinerasoanambinina@gmail.com>
2025-12-08 17:32:59 +0300

    nouveau

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 # wait-for-db.sh
-
 set -e
 
-host="$1"
+host="$1"      # Nom ou IP de la base
 shift
-cmd="$@"
+cmd="$@"       # Commande à exécuter après que la DB soit disponible
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+echo "Waiting for database at $host..."
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
   echo "Postgres is unavailable - sleeping"
   sleep 2
 done
 
+echo "Postgres is available - starting application..."
 exec $cmd
